6a19c70f1bfabf8422ea9aa81df7cf9e28fbeede
amo <lwllai@sina.com>
2020-07-26 22:23:07 +0800

    编译报错先提交后详细缓慢修改

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import i18n from '@/locale'
+import iView from 'view-design'
 
 Vue.config.productionTip = false
 
+Vue.use(iView, {
+  i18n: (key, value) => i18n.t(key, value)
+})
+
+/* eslint-disable no-new */
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router,
+  i18n,
+  store,
+  render: h => h(App)
+})
