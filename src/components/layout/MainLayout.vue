335f7933841f7086f1353a10e3e99d52de23c1fa
amo <lwllai@sina.com>
2020-07-28 22:33:22 +0800

    正式提交需改正css为less学习

---
+++ src/components/layout/MainLayout.vue
@@ -0,0 +1,18 @@
+<template>
+    <Layout class="i-lauout">
+
+    </Layout>
+</template>
+
+<script>
+
+    import './MainLayout.css'
+
+    export default {
+        name: "MainLayout"
+    }
+</script>
+
+<style scoped>
+
+</style>
